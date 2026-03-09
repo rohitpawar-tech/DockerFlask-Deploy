f8d62fb461131d9710e77994b2fa52766666767b
Rohit Pawar <rp035806@gmail.com>
2026-03-10 00:08:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,11 @@
 from flask import Flask, jsonify
 import logging
 import os
+app = Flask(__name__)
 
+# Configure Logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
